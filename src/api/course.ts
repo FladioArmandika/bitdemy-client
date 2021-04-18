bb1ec5c6ddcea672ae2d8b32c331ccf4bd76d15f
fladioarmandika <fladioarmandika@gmail.com>
2021-04-18 20:21:44 +0700

    ✨ add course get api

---
+++ src/api/course.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
+import { API_COURSE_GET } from "../constants/api"
+
+export default class CourseAPI {
+
+    public getCourse = (courseId: string) =>  {
+        const config: AxiosRequestConfig = {
+            url: API_COURSE_GET.url,
+            params: { courseId }
+        }
+
+        return axios(config)
+            .then( (response: AxiosResponse) => {
+                if (response.status === 200) {
+                    return response.data
+                }
+            })
+    }
+  
+}
