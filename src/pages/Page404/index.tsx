cb3ff6ef4b5847e3d31374f671c4c22b9c1f3ce2
fladioarmandika <fladioarmandika@gmail.com>
2021-04-24 23:30:16 +0700

    💄 add 404 page

---
+++ src/pages/Page404/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function Page404(props: Props): ReactElement {
+    return (
+        <div>
+            404 PAGE NOT FOUND
+        </div>
+    )
+}
